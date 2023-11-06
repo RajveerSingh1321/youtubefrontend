4b8c98b232277f0f9c08453b04f09ae55243ff95
RajveerSingh1321 <rajveersingh391717@gmail.com>
2023-11-06 13:38:39 +0530

    new update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
+// import logo from './logo.svg';
+// import './App.css';
+// import TableComponent from "./components/table";
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <TableComponent></TableComponent>
+//       <TableComponent></TableComponent>
+//       <TableComponent></TableComponent>
+//       <TableComponent></TableComponent>
+//       <TableComponent></TableComponent>
+//       <TableComponent></TableComponent>
+
+
+
+//     </div> 
+//   );
+// }
+    
+
+
+// export default App;
 import logo from './logo.svg';
 import './App.css';
-
+// import TableComponent from "./components/table";
+import{Routes,Route,BrowserRouter}from "react-router-dom";
+// import SomeComponent from "./components/somecomponents";
+// import HookComponent from './components/hookcomponents';
+// import Timebomb from './components/timebomb';
+import HomePage from './components/HomePage';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    // <TableComponent></TableComponent>
+    // <TableComponent></TableComponent>
+    // <TableComponent></TableComponent>
+    
+    // </div>
+
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path='/login' element={<TableComponent />}></Route>
+        <Route path='/helo' element={<SomeComponent />}></Route>
+        <Route path='/hook' element={<HookComponent />}></Route>
+        <Route path='/timebomb' element={<Timebomb />}></Route> */}
+          <Route path="/" element={<HomePage />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/VideoPage.js
@@ -0,0 +1,8 @@
+import { BrowserRouter } from "react-router-dom";
+const VideoPage=()=>{
+    return(
+        <BrowserRouter>
+        
+        </BrowserRouter>
+    )
+}
